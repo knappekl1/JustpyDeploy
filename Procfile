4d64e86db5e12cc3bc37b7b1219594e081efe9f4
Libor Knappek <libor.knappek@gmail.com>
2021-06-08 12:59:07 +0200

    testing

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn main_app:jp.justpy(my_web)
---
+++ main_app.py
@@ -0,0 +1,9 @@
+import justpy as jp
+
+def my_web():
+
+    wp = jp.WebPage()
+    h1 = jp.Div(a=wp, text="Hello world", classes="text-center w-1/2 bg-red-400")
+    return wp
+
+jp.justpy(my_web)
